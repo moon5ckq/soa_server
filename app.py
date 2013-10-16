83a9ac9365014d8dba5ee142bf347cc6d4046d26
moon5ckq <moon5ckq@gmail.com>
2013-10-16 13:44:12 +0800

    add an other aminer service to calc the result.

--- app.py
+++ app.py
@@ -5,23 +5,70 @@ import json
 app = Flask(__name__)
 app.debug = True
 
+g_result = {}
+
 @app.route("/")
 def hello():
 	return "hello world"
 
-URL_SEARCH_EXPERT = "http://arnetminer.org/services/search-expert?u=mlckq&start=0&num=100&q=%s"
-URL_SEARCH_PUBLICATION = "http://arnetminer.org/services/search-publication?u=mlckq&start=0&num=100&q=%s"
+URL_SEARCH_EXPERT = "http://arnetminer.org/services/search-expert?u=mlckq&start=0&num=10000&q=%s"
+URL_SEARCH_PUBLICATION = "http://arnetminer.org/services/search-publication?u=mlckq&start=0&num=10000&q=%s"
 URL_SEARCH_CONFERENCE = "http://arnetminer.org/services/search-conference?u=mlckq&start=0&num=100&q=%s"
 URL_SEARCH_PUBLICATION_BY_AUTHOR = "http://arnetminer.org/services/publication/byperson/%s"
+
+def my_comp(u, v):
+	u1 = 0
+	if g_result[u].has_key("Citedby"):
+		u1 += g_result[u]["Citedby"] * 0.023
+	if g_result[u].has_key("rank"):
+		u1 += 10.0 / (g_result[u]["rank"] + 1)
+	if g_result[u].has_key("Edge"):
+		u1 += g_result[u]["Edge"] * 0.01
+	
+	v1 = 0
+	if g_result[v].has_key("Citedby"):
+		v1 += g_result[v]["Citedby"] * 0.023
+	if g_result[v].has_key("rank"):
+		v1 += 10.0 / (g_result[v]["rank"] + 1)
+	if g_result[v].has_key("Edge"):
+		v1 += g_result[v]["Edge"] * 0.01
+	
+	ret = 0
+	if u1 == v1:
+		ret = 0
+	elif u1 > v1:
+		ret = -1
+	else:
+		ret = 1
+	return ret
+
 
 @app.route("/search/<query>")
 def search(query):
 	response = urllib2.urlopen((URL_SEARCH_EXPERT % query).replace(" ","%20")).read()
-	data = json.loads(response)
-	result = []
-	for item in data["Results"]:
-		result.append(item["Id"])
-	return json.dumps(result)	
+	data_exp = json.loads(response)["Results"]
+	response = urllib2.urlopen((URL_SEARCH_PUBLICATION % query).replace(" ","%20")).read()
+	data_pub = json.loads(response)["Results"]
+	result = {}
+	for i in xrange(0, len(data_exp)):
+		result[data_exp[i]["Id"]] = {"rank" : i}
+	for pub in data_pub:
+		if pub.has_key("AuthorIds"):
+			for author in pub["AuthorIds"]:
+				if not result.has_key(author):
+					result[author] = {}
+				if not result[author].has_key("Citedby"):
+					result[author]["Citedby"] = 0
+				if not result[author].has_key("Edge"):
+					result[author]["Edge"] = 0
+				result[author]["Citedby"] += pub["Citedby"]
+				result[author]["Edge"] += len(pub["AuthorIds"]) - 1
+	global g_result
+	g_result = result
+	result = g_result.keys()
+	result = sorted(result, my_comp)
+
+	return "<pre>" + json.dumps(result, indent = 4)	+ "</pre>"
 
 def rerank(result):
 	better_result = result
@@ -58,7 +105,7 @@ def network(query):
 	for e in edges:
 		x = e.split("-")
 		result["links"].append({"source":author_index[int(x[0])], "target":author_index[int(x[1])], "value":edges[e]})
-	return json.dumps(result)
+	return "<pre>" + json.dumps(result, indent=4) + "</pre>"
 
 @app.route("/coauthor/")
 def coauthor():
